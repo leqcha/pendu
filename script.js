d09468738b1b5ab752d1042a1b31b84455b6945d
charlotte <charlotte.lequesne@hotmail.fr>
2023-11-02 19:28:55 +0100

    ajout gestionnaire d'événement sur le clavier

--- script.js
+++ script.js
@@ -81,19 +81,10 @@ function nouvellePartie() {
 
 nouvellePartie();
 
-// Écrivez une fonction pour gérer le clic sur une lettre du clavier virtuel et vérifier si la lettre est correcte.
-
-// Gérez le décompte des erreurs et l'affichage progressif de l'image du pendu.
-// Vérifiez si le joueur a gagné ou perdu la partie.
-// Affichez des messages d'état en fonction du déroulement du jeu.
-// Étape 5 : Interaction utilisateur
-
-// Ajoutez des gestionnaires d'événements pour le bouton "Nouvelle Partie" et les lettres du clavier virtuel.
-
-// Étape 7 : Personnalisation et améliorations (facultatif)
-
-// Ajoutez des fonctionnalités supplémentaires comme un chronomètre, des niveaux de difficulté, ou un classement en ligne.
-// Personnalisez davantage les styles et l'interface utilisateur.
-
-// function qui écrit le motAleatoire dans les tirets. chaque lettre représente 1 tiret
-// Créer un tableau qui comporte 1 index de lettre
+let lettre = document.querySelectorAll('td');
+lettre.forEach(function(e){
+    e.addEventListener('click', function() {
+        lettre = e.textContent;
+        console.log(lettre);
+    });
+});
