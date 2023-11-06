bf8331cfe126bedd8bb66df92b4a81d4353c736d
leqcha <charlotte.lequesne@hotmail.fr>
2023-11-06 17:04:05 +0100

    fonctionnel en console

--- script.js
+++ script.js
@@ -1,60 +1,63 @@
 let motAleatoire = '';
-let tirets = '';
+let tirets = [];
 let motADeviner = [];
+let coups = 7;
+let numeroImage = 0;
+let lettre = '';
+
+let score = document.querySelector('.score');
+let image = document.querySelector('img');
 
 // Liste de mots
 let tableauDeMots = [
-    "Banane",
-    "Chien",
-    "Chat",
-    "Maison",
-    "Avion",
-    "Pomme",
-    "Table",
-    "Orange",
-    "Soleil",
-    "Lune",
-    "Voiture",
-    "Gâteau",
-    "École",
-    "Montagne",
-    "Plage",
-    "Fleurs",
-    "Musique",
-    "Livre",
-    "Football",
-    "Basket",
-    "Ordinateur",
-    "Téléphone",
-    "Pizza",
-    "Robot",
-    "Dinosaure",
-    "Éléphant",
-    "Girafe",
-    "Oiseau",
-    "Papillon",
-    "Arbre",
-    "Coccinelle",
-    "Lapin",
-    "Dauphin",
-    "Sirène",
-    "Fusée",
-    "Hélicoptère",
-    "Licorne",
-    "Dragon",
-    "Fraise",
-    "Télévision",
-    "Fusil",
-    "Tarte",
-    "Étoile",
-    "Guitare",
-    "Train",
-    "Bateau",
-    "Guitare",
-    "Piano",
-    "Piscine",
-    "Horloge"
+    "chien", "chat", "table", "ordinateur", "maison", "voiture", "fenetre", "porte", "fleur", "arbre",
+    "soleil", "lune", "étoile", "plage", "mer", "montagne", "riviere", "pont", "nuage", "herbe",
+    "cafe", "livre", "stylo", "papier", "musique", "film", "fenetre", "vélo", "ballon", "portable",
+    "ordinateur", "ecran", "clavier", "souris", "ecouteurs", "casque", "telecommande", "pizza", "hamburger", "sandwich",
+    "chocolat", "gateau", "bonbon", "glace", "soda", "eau", "jus", "orange", "pomme"
 ];
+
+// Suppression des accents et des majuscules
+// function supprimerAccents(str) {
+//     return str.normalize("NFD").replace(/[\u0300-\u036f]/g, "");
+// }
+// let tableauDeMotsSansAccents = tableauDeMots.map(mot => supprimerAccents(mot.toLowerCase()));
+// console.log(tableauDeMotsSansAccents);
+
+
+// Générer une nouvelle partie au clic sur le bouton nouvelle partie
+let nouvellePartieBtn = document.querySelector('.nouvellePartie');
+nouvellePartieBtn.addEventListener('click', nouvellePartie);
+
+function nouvellePartie() {
+    // remise à 0 des variables et fonctions
+    document.querySelector('.tirets').textContent = '';
+    numeroImage = 0;
+    image.src = `images/${numeroImage}.png`;
+    coups = 7;
+    score.textContent = coups;
+    tirets = [];
+    motADeviner = [];
+
+    motADeviner = genererMot(tableauDeMots.length);
+    console.log(motADeviner);
+    copieMotAleatoire();
+    genererTirets();
+}
+
+function copieMotAleatoire(){
+    motADeviner = motAleatoire.split('');
+    console.log(motADeviner);
+}
+
+// Générer des tirets pour le tableau copieMotAleatoire
+function genererTirets() {
+    for (let i = 0; i < motAleatoire.length; i++) {
+        tirets = document.querySelector('.tirets').append(' _ ');
+    }
+}
+
+
 
 // Choisir un mot aléatoire dans la liste et le retourner sous forme de tableau
 function genererMot(max) {
@@ -67,24 +70,38 @@ function genererMot(max) {
     return motADeviner;
 }
 
-// Générer le nombre de tirets correspondant au nombre de lettres
-function genererTirets() {
-    for (let i = 0; i < motAleatoire.length; i++) {
-        tirets = document.querySelector('.tirets').append(' _ ');
-    }
-}
 
-function nouvellePartie() {
-    console.log(genererMot(tableauDeMots.length));
-    genererTirets();
+let lettreElements = document.querySelectorAll('td');
+lettreElements.forEach(function (e) {
+    e.addEventListener('click', function () {
+        lettre = e.textContent;
+        console.log(lettre);
+        verifierLettre(lettre);
+    });
+});
+
+function verifierLettre(lettre) {
+    let lettreTrouvee = false;
+
+    for (let i = 0; i < motADeviner.length; i++) {
+        if (motADeviner[i] === lettre) {
+            lettreTrouvee = true;
+        }
+    }
+
+    if (lettreTrouvee) {
+        console.log('la lettre est dans le mot');
+        score.textContent = coups;
+    } else if (coups === 0 || coups < 0) {
+        alert('perdu');
+        image.src = 'images/7.png';
+    } else {
+        console.log('la lettre n\'est pas dans le mot');
+        coups--;
+        score.textContent = coups;
+        image.src = `images/${++numeroImage}.png`;
+    }
+
 }
 
 nouvellePartie();
-
-let lettre = document.querySelectorAll('td');
-lettre.forEach(function(e){
-    e.addEventListener('click', function() {
-        lettre = e.textContent;
-        console.log(lettre);
-    });
-});
